ec6106f52c0422e527bfdd6fbd33b430a6160cdc
Yilin Gui <yilin.gui@gmail.com>
2021-07-13 22:04:29 +0800

    Update git_count_lines.sh

--- shell/git_count_lines.sh
+++ shell/git_count_lines.sh
@@ -1,2 +1,11 @@
-git log --author="guiyilin" --pretty=tformat: --numstat \
+#bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 AUTHOR_NAME"
+    exit 1
+fi
+
+AUTHOR_NAME=$1
+
+git log --author=$AUTHOR_NAME --pretty=tformat: --numstat \
 | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s removed lines: %s total lines: %s\n", add, subs, loc }' -
